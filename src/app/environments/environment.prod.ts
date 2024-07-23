e598fbc534096c3292d506d3fd8178bc28dfaef8
jdmarvin7 <jdmarvinmaeldestin02@gmail.com>
2024-07-23 19:54:08 -0300

    021: Tirando token de prod

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment= {
-    googleApiKey: (window['env' as any] as any)?.GOOGLE_API_KEY || 'AIzaSyDaYxWkXa4TmET6fBVin1q4oYNzpJgE_qUs',
+    googleApiKey: (window['env' as any] as any)?.GOOGLE_API_KEY || '',
     production: true,
 }
