4dd7bae6e23d7bf1d8cd2cef90c97d7bb379d3b2
jdmarvin7 <jdmarvinmaeldestin02@gmail.com>
2024-07-23 19:29:39 -0300

    Fora do range

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    googleApiKey: (window['env' as any] as any)?.GOOGLE_API_KEY || '',
+    googleApiKey: (window['env' as any] as any)?.GOOGLE_API_KEY || 'AIzaSyDaYxWkXa4TmET6fBVin1q4oYNzpJgE_qU',
     production: false
 }
 
