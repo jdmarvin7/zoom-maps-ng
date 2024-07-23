c7c4fd9020b1773d4730c07141d04cc48add2c7d
jdmarvin7 <jdmarvinmaeldestin02@gmail.com>
2024-07-23 01:13:04 -0300

    013: Lazy loading api key

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    googleApiKey: (window['env' as any] as any)?.GOOGLE_API_KEY || 'AIzaSyDaYxWkXa4TmET6fBVin1q4oYNzpJgE_qU',
+    googleApiKey: (window['env' as any] as any)?.GOOGLE_API_KEY || '',
 }
